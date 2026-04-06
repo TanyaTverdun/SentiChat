b5f48a385a150b726b3ccf6539bb4a1ef9ba430f
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 01:16:24 +0300

    Create Message entity

---
+++ SentiChat.Domain/Entities/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace SentiChat.Domain.Entities;
+
+public class Message
+{
+    public Guid Id { get; set; }
+
+    public Guid ChatId { get; set; }
+    public Chat Chat { get; set; } = null!;
+
+    public Guid SenderId { get; set; }
+    public User Sender { get; set; } = null!;
+
+    public string Text { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public string? SentimentResult { get; set; }
+}
