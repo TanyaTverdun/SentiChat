72be353c6e119809db6e3a35dd52684136017f9f
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 01:43:16 +0300

    Create ChatConfiguration

---
+++ SentiChat.Infrastructure/Data/Configurations/ChatConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SentiChat.Domain.Entities;
+
+namespace SentiChat.Infrastructure.Data.Configurations;
+
+public class ChatConfiguration : IEntityTypeConfiguration<Chat>
+{
+    public void Configure(EntityTypeBuilder<Chat> builder)
+    {
+        builder
+            .HasKey(c => c.Id);
+
+        builder
+            .Property(c => c.Title)
+            .HasMaxLength(100)
+            .IsUnicode(true);
+
+        builder
+            .Property(c => c.IsGroup)
+            .IsRequired();
+    }
+}
