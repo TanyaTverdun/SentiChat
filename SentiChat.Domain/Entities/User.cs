a09027411bc2fbfc774ac98566bfc1e9054ce66e
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 01:08:51 +0300

    Create User entity

---
+++ SentiChat.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace SentiChat.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string? Bio { get; set; }
+    public string PasswordHash { get; set; }
+
+    public ICollection<ChatMember> ChatMembers { get; set; } = new List<ChatMember>();
+    public ICollection<Message> SentMessages { get; set; } = new List<Message>();
+}
