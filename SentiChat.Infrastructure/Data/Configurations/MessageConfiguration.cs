8cdadf5c1b1aa61b65f7d899fd94fb07bcfe96e6
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 01:53:59 +0300

    Create MessageConfiguration

---
+++ SentiChat.Infrastructure/Data/Configurations/MessageConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SentiChat.Domain.Entities;
+
+namespace SentiChat.Infrastructure.Data.Configurations;
+
+public class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder
+            .HasKey(m => m.Id);
+
+        builder
+            .Property(m => m.Text)
+            .HasMaxLength(4000)
+            .IsUnicode(true)
+            .IsRequired();
+
+        builder
+            .Property(m => m.CreatedAt)
+            .HasColumnType("datetime2")
+            .IsRequired();
+
+        builder
+            .Property(m => m.SentimentResult)
+            .HasMaxLength(50)
+            .IsUnicode(false);
+
+        builder
+            .HasOne(m => m.Sender)
+            .WithMany(u => u.SentMessages)
+            .HasForeignKey(m => m.SenderId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(m => m.Chat)
+            .WithMany(c => c.Messages)
+            .HasForeignKey(m => m.ChatId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
