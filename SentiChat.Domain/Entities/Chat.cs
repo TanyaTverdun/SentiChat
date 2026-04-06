5d9dc4419a1aae361a45df18a62fedb37457ddc2
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 01:10:05 +0300

    Create Chat entity

---
+++ SentiChat.Domain/Entities/Chat.cs
@@ -0,0 +1,11 @@
+﻿namespace SentiChat.Domain.Entities;
+
+public class Chat
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsGroup { get; set; }
+
+    public ICollection<ChatMember> Members { get; set; } = new List<ChatMember>();
+    public ICollection<Message> Messages { get; set; } = new List<Message>();
+}
