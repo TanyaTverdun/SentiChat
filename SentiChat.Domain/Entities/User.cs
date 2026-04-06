f4c382bf58b7272903c3850274da31924bb9eae8
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 01:14:25 +0300

    Initialize User entity string properties with string.Empty

--- SentiChat.Domain/Entities/User.cs
+++ SentiChat.Domain/Entities/User.cs
@@ -3,10 +3,10 @@
 public class User
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string? Bio { get; set; }
-    public string PasswordHash { get; set; }
+    public string PasswordHash { get; set; } = string.Empty;
 
     public ICollection<ChatMember> ChatMembers { get; set; } = new List<ChatMember>();
     public ICollection<Message> SentMessages { get; set; } = new List<Message>();
