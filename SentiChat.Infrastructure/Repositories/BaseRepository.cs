83ab84a571a67d59dc1bebcaed0a2bc347d7cadc
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 20:08:21 +0300

    Create generic BaseRepository

---
+++ SentiChat.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SentiChat.Domain.Interfaces.Repositories;
+
+namespace SentiChat.Infrastructure.Repositories;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    protected readonly DbContext _dbContext;
+    protected readonly DbSet<T> _dbSet;
+
+    public BaseRepository(DbContext dbContext)
+    {
+        this._dbContext = dbContext;
+        this._dbSet = _dbContext.Set<T>();
+    }
+
+    public async Task AddAsync(T entity, CancellationToken cancellationToken)
+    {
+        await this._dbSet.AddAsync(entity, cancellationToken);
+    }
+}
