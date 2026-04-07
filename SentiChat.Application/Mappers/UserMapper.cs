d221e240b611ec1e0b24cbfe7a045d73502bb29f
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 20:38:40 +0300

    Add UserMapper to map DTO to entity

---
+++ SentiChat.Application/Mappers/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using Riok.Mapperly.Abstractions;
+using SentiChat.Application.DTOs.Users;
+using SentiChat.Domain.Entities;
+
+namespace SentiChat.Application.Mappers;
+
+[Mapper]
+public static partial class UserMapper
+{
+    [MapperIgnoreTarget(nameof(User.Bio))]
+    [MapperIgnoreTarget(nameof(User.SentMessages))]
+    [MapperIgnoreTarget(nameof(User.ChatMembers))]
+    [MapperIgnoreSource(nameof(RegisterUserDto.Password))]
+    public static partial User ToEntity(this RegisterUserDto dto, Guid id, string passwordHash);
+}
