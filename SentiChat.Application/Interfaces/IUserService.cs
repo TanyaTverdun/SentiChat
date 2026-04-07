23c0a5aaa0718797e80ce18bc178582ccaec2f0d
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 20:36:48 +0300

    Add IUserService interface

---
+++ SentiChat.Application/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using SentiChat.Application.DTOs.Users;
+
+namespace SentiChat.Application.Interfaces;
+
+/// <summary>
+/// Defines the contract for user-related business operations.
+/// </summary>
+public interface IUserService
+{
+    /// <summary>
+    /// Registers a new user in the system.
+    /// </summary>
+    /// <param name="registerUser">The data transfer object containing user registration details.</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>The unique identifier (GUID) of the newly registered user.</returns>
+    /// <exception cref="ArgumentException">Thrown when a user with the provided email already exists.</exception>
+    public Task<Guid> RegisterUserAsync(RegisterUserDto registerUser, CancellationToken cancellationToken);
+}
