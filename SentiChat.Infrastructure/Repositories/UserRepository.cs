4d9f7eb60982dc9a584a29f93c71810326d4c296
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 20:26:24 +0300

    Add UserRepository implementation

---
+++ SentiChat.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SentiChat.Domain.Entities;
+using SentiChat.Domain.Interfaces.Repositories;
+using SentiChat.Infrastructure.Data;
+
+namespace SentiChat.Infrastructure.Repositories;
+
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    public UserRepository(SentiChatDbContext dbContext) 
+        : base(dbContext)
+    {
+    }
+
+    public async Task<bool> ExistsByEmailAsync(
+        string email, 
+        CancellationToken cancellationToken)
+    {
+        return await this._dbSet
+            .AnyAsync(
+                u => u.Email == email, 
+                cancellationToken);
+    }
+}
