4eaceb1179fca8fef04a02d5737dd86542ec37b9
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 01:13:53 +0300

    Add LoginUserAsync method to service interface

--- SentiChat.Application/Interfaces/IUserService.cs
+++ SentiChat.Application/Interfaces/IUserService.cs
@@ -15,4 +15,13 @@ public interface IUserService
     /// <returns>The unique identifier (GUID) of the newly registered user.</returns>
     /// <exception cref="ArgumentException">Thrown when a user with the provided email already exists.</exception>
     public Task<Guid> RegisterUserAsync(RegisterUserDto registerUser, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Authenticates a user based on the provided credentials.
+    /// </summary>
+    /// <param name="loginDto">The data transfer object containing login credentials (email and password).</param>
+    /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+    /// <returns>The unique identifier (GUID) of the authenticated user.</returns>
+    /// <exception cref="UnauthorizedAccessException">Thrown when the email is not found or the password does not match.</exception>
+    public Task<Guid> LoginUserAsync(LoginUserDto loginUserDto, CancellationToken cancellationToken);
 }
