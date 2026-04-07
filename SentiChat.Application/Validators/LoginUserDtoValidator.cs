ff4f55d41ea0327d742dff8200e35ae70d2a3867
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 00:49:16 +0300

    Create LoginUserDtoValidator

---
+++ SentiChat.Application/Validators/LoginUserDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SentiChat.Application.DTOs.Users;
+
+namespace SentiChat.Application.Validators;
+
+public class LoginUserDtoValidator : AbstractValidator<LoginUserDto>
+{
+    public LoginUserDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Invalid email format.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("Password is required.");
+    }
+}
