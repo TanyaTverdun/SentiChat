fcd098c9153d40d3bfe35329b942a1d05249614e
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 00:46:18 +0300

    Create LoginUserDto

---
+++ SentiChat.Application/DTOs/Users/LoginUserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SentiChat.Application.DTOs.Users;
+
+public class LoginUserDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
