2ee5cd5c74207a62ca476fc499ded5072c08e1a6
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 00:40:04 +0300

    Create ExceptionMiddleware
    Added ExceptionMiddleware to handle global exceptions

---
+++ SentiChat/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace SentiChat.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddleware(
+        RequestDelegate next, 
+        ILogger<ExceptionMiddleware> logger)
+    {
+        this._next = next;
+        this._logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            this._logger
+                .LogError(
+                    ex, 
+                    "An unexpected error occurred: {Message}.", 
+                    ex.Message);
+
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(
+        HttpContext context, 
+        Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        int statusCode;
+        string errorMessage;
+
+        switch (exception)
+        {
+            case ArgumentException:
+                statusCode = (int)HttpStatusCode.BadRequest;
+                errorMessage = exception.Message;
+                break;
+
+            default:
+                statusCode = (int)HttpStatusCode.InternalServerError;
+                errorMessage = "An unexpected error occurred.";
+                break;
+        }
+
+        context.Response.StatusCode = statusCode;
+        var result = JsonSerializer.Serialize(new
+        {
+            Error = errorMessage
+        });
+
+        return context.Response.WriteAsync(result);
+    }
+}
