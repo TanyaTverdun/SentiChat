717baca13d79c528aa24ba6648e5f98462609a2f
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 15:01:14 +0300

    Create IUserRepository interface

---
+++ SentiChat.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using SentiChat.Domain.Entities;
+
+namespace SentiChat.Domain.Interfaces.Repositories;
+
+/// <summary>
+/// Repository interface for User entity operations.
+/// </summary>
+public interface IUserRepository : IBaseRepository<User>
+{
+    /// <summary>
+    /// Checks if a user with the specified email already exists.
+    /// </summary>
+    public Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken);
+}
