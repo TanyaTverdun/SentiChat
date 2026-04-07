bb893d075de2a64e3dff06535c78dfff8ae60ebc
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 20:11:03 +0300

    Add UnitOfWork implementation

---
+++ SentiChat.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using SentiChat.Domain.Interfaces.Repositories;
+using SentiChat.Infrastructure.Data;
+
+namespace SentiChat.Infrastructure.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly SentiChatDbContext _dbContext;
+
+    public UnitOfWork(SentiChatDbContext dbContext)
+    {
+        this._dbContext = dbContext;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await this._dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
