6e8309935561e397e351c6f2f40c2b5f662d2483
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 00:41:50 +0300

    Create DependencyInjection files for repositories and services

---
+++ SentiChat.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using SentiChat.Application.Interfaces;
+using SentiChat.Application.Services;
+using SentiChat.Application.Validators;
+
+namespace SentiChat.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+        services.AddValidatorsFromAssemblyContaining<RegisterUserDtoValidator>();
+
+        return services;
+    }
+}
---
+++ SentiChat.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SentiChat.Application.Interfaces.Security;
+using SentiChat.Domain.Interfaces.Repositories;
+using SentiChat.Infrastructure.Repositories;
+using SentiChat.Infrastructure.Security;
+
+namespace SentiChat.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+        return services;
+    }
+}
