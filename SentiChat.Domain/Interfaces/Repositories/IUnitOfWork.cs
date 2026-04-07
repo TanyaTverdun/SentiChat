b0d08cb2aa2a973854e43c8057890cdebc1840b1
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 14:59:53 +0300

    Add IUnitOfWork interface

---
+++ SentiChat.Domain/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace SentiChat.Domain.Interfaces.Repositories;
+
+/// <summary>
+/// Interface for managing database transactions.
+/// </summary>
+public interface IUnitOfWork
+{
+    /// <summary>
+    /// Saves changes to the database.
+    /// </summary>
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
