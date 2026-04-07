a49d5426091e2bca9548377636906c1b6c43553f
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 20:32:30 +0300

    Add PasswordHasher implementation

---
+++ SentiChat.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using SentiChat.Application.Interfaces.Security;
+
+namespace SentiChat.Infrastructure.Security;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string hashedPassword, string providedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword);
+    }
+}
