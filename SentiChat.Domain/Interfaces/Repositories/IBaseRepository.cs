51bb3a01a2c7eceb9ebb4e01cecf67a7ed1491bd
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-07 14:51:58 +0300

    Add base generic repository interface

---
+++ SentiChat.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SentiChat.Domain.Interfaces.Repositories;
+
+/// <summary>
+/// Base generic repository for common data operations.
+/// </summary>
+/// <typeparam name="T">The entity type.</typeparam>
+public interface IBaseRepository<T> where T : class
+{
+    /// <summary>
+    /// Adds a new entity to the data store.
+    /// </summary>
+    Task AddAsync(T entity, CancellationToken cancellationToken);
+}
