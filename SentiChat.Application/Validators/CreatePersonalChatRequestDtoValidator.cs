53b7c1597159721169b223805230519ba256eb51
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 19:35:23 +0300

    Add CreatePersonalChatRequestDtoValidator

---
+++ SentiChat.Application/Validators/CreatePersonalChatRequestDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using SentiChat.Application.DTOs.Chats;
+
+namespace SentiChat.Application.Validators;
+
+public class CreatePersonalChatRequestDtoValidator 
+    : AbstractValidator<CreatePersonalChatRequestDto>
+{
+    public CreatePersonalChatRequestDtoValidator()
+    {
+        RuleFor(x => x.TargetUserEmail)
+            .NotEmpty()
+            .WithMessage("Target user email is required.")
+            .EmailAddress()
+            .WithMessage("A valid email address is required.");
+    }
+}
