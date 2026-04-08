0b58ab8ff3bf985212bb257209b3234a6349d9ae
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-09 01:26:25 +0300

    Add MessageMapper

---
+++ SentiChat.Application/Mappers/MessageMapper.cs
@@ -0,0 +1,43 @@
+﻿using Riok.Mapperly.Abstractions;
+using SentiChat.Application.DTOs.Messages;
+using SentiChat.Domain.Entities;
+using SentiChat.Domain.Enums;
+
+namespace SentiChat.Application.Mappers;
+
+[Mapper]
+public static partial class MessageMapper
+{
+    public static Message ToEntity(
+        Guid id,
+        Guid chatId,
+        Guid senderId,
+        string content,
+        DateTime createdAt,
+        SentimentType sentiment)
+    {
+        return new Message
+        {
+            Id = id,
+            ChatId = chatId,
+            SenderId = senderId,
+            Text = content,
+            CreatedAt = createdAt,
+            Sentiment = sentiment
+        };
+    }
+
+    [MapperIgnoreSource(nameof(Message.Chat))]
+    [MapperIgnoreSource(nameof(Message.ChatId))]
+    [MapperIgnoreSource(nameof(Message.Sender))]
+    [MapProperty(nameof(Message.Text), nameof(MessageDto.Content))]
+    [MapProperty(nameof(Message.CreatedAt), nameof(MessageDto.SentAt))]
+    [MapProperty(nameof(Message.Sentiment), nameof(MessageDto.Sentiment))]
+    public static partial MessageDto ToDto(this Message message);
+
+    public static partial IEnumerable<MessageDto> ToDtoList(
+        this IEnumerable<Message> messages);
+
+    private static string MapSentimentToString(
+        SentimentType sentiment) => sentiment.ToString();
+}
