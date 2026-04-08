6c0b4791bac8f61327e28b437062a05d2287c926
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-09 01:27:05 +0300

    Add SendMessageRequestValidator

---
+++ SentiChat.Application/Validators/SendMessageRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using SentiChat.Application.DTOs.Messages;
+
+namespace SentiChat.Application.Validators;
+
+public class SendMessageRequestValidator : AbstractValidator<SendMessageRequestDto>
+{
+    public SendMessageRequestValidator()
+    {
+        RuleFor(x => x.Content)
+             .NotEmpty()
+             .WithMessage("Message content cannot be empty.")
+             .MaximumLength(2000)
+             .WithMessage("Message is too long (maximum 2000 characters).");
+    }
+}
