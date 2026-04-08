c0739949d8fc3560c74ab68a96f4f29758163302
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 21:58:31 +0300

    Add IMessageRepository and its implementation

---
+++ SentiChat.Infrastructure/Repositories/MessageRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SentiChat.Domain.Entities;
+using SentiChat.Domain.Interfaces.Repositories;
+using SentiChat.Infrastructure.Data;
+
+namespace SentiChat.Infrastructure.Repositories;
+
+public class MessageRepository : BaseRepository<Message>, IMessageRepository
+{
+    public MessageRepository(SentiChatDbContext context) 
+        : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Message>> GetMessagesAsync(
+        Guid chatId, 
+        int pageSize, 
+        DateTime? before, 
+        CancellationToken cancellationToken)
+    {
+        IQueryable<Message> query = this._dbSet
+            .Where(m => m.ChatId == chatId);
+
+        if (before.HasValue)
+        {
+            query = query.Where(m => m.CreatedAt < before.Value);
+        }
+
+        return await query
+            .OrderByDescending(m => m.CreatedAt)
+            .Take(pageSize)
+            .OrderBy(m => m.CreatedAt)
+            .ToListAsync(cancellationToken);
+    }
+}
