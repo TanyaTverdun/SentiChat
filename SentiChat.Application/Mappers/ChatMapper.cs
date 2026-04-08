c1021419cba7baf8be9b100f72cc9183269dfafb
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 14:19:15 +0300

    Add ChatMapper

---
+++ SentiChat.Application/Mappers/ChatMapper.cs
@@ -0,0 +1,21 @@
+﻿using Riok.Mapperly.Abstractions;
+using SentiChat.Application.DTOs.Chats;
+using SentiChat.Domain.Entities;
+
+namespace SentiChat.Application.Mappers;
+
+[Mapper]
+public static partial class ChatMapper
+{
+    [MapProperty(nameof(Chat.Id), nameof(ChatListItemDto.ChatId))]
+    [MapperIgnoreSource(nameof(Chat.Title))]
+    [MapperIgnoreSource(nameof(Chat.IsGroup))]
+    [MapperIgnoreSource(nameof(Chat.Messages))]
+    [MapperIgnoreSource(nameof(Chat.Members))]
+    public static partial ChatListItemDto MapToListItemDto(
+        this Chat chat,
+        string name,
+        string initials,
+        string? lastMessageText,
+        DateTime? lastMessageTime);
+}
