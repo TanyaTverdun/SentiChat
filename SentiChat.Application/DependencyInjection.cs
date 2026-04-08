db81528c3872842aa2cd9774baf19e2baa0216db
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 19:39:52 +0300

    Add ChatService to DependencyInjection

--- SentiChat.Application/DependencyInjection.cs
+++ SentiChat.Application/DependencyInjection.cs
@@ -11,6 +11,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IChatService, ChatService>();
 
         services.AddValidatorsFromAssemblyContaining<RegisterUserDtoValidator>();
 
