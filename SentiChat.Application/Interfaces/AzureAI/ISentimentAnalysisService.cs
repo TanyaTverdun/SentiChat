0fd0bcf6fb1106a9ccba425438f122b166891a72
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 22:27:32 +0300

    add ISentimentAnalysisService and its implementation

---
+++ SentiChat.Application/Interfaces/AzureAI/ISentimentAnalysisService.cs
@@ -0,0 +1,17 @@
+﻿using SentiChat.Domain.Enums;
+
+namespace SentiChat.Application.Interfaces.AzureAI
+{
+    /// <summary>
+    /// Defines a contract for analyzing the emotional tone of text.
+    /// </summary>
+    public interface ISentimentAnalysisService
+    {
+        /// <summary>
+        /// Analyzes the sentiment of the provided text.
+        /// </summary>
+        /// <param name="text">The message content to analyze.</param>
+        /// <returns>The detected sentiment type (Positive, Negative, Neutral, or Mixed).</returns>
+        Task<SentimentType> AnalyzeSentimentAsync(string text);
+    }
+}
---
+++ SentiChat.Infrastructure/ExternalServices/AzureAI/SentimentAnalysisService.cs
@@ -0,0 +1,56 @@
+﻿using SentiChat.Application.Interfaces.AzureAI;
+using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.Extensions.Configuration;
+using SentiChat.Domain.Enums;
+
+namespace SentiChat.Infrastructure.ExternalServices.AzureAI;
+
+public class SentimentAnalysisService : ISentimentAnalysisService
+{
+    private readonly TextAnalyticsClient _client;
+
+    public SentimentAnalysisService(IConfiguration configuration)
+    {
+        var endpoint = configuration["Azure:LanguageService:Endopoint"];
+        var apiKey = configuration["Azure:LanguageService:ApiKey"];
+
+        if(string.IsNullOrEmpty(endpoint) || string.IsNullOrEmpty(apiKey))
+        {
+            throw new InvalidOperationException(
+                "Azure AI settings are missing in configuration.");
+        }
+
+        var credentials = new AzureKeyCredential(apiKey);
+        this._client = new TextAnalyticsClient(
+            new Uri(endpoint), 
+            credentials);
+    }
+
+    public async Task<SentimentType> AnalyzeSentimentAsync(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return SentimentType.Neutral;
+        }
+
+        try
+        {
+            DocumentSentiment response = await this._client
+                .AnalyzeSentimentAsync(text);
+
+            return response.Sentiment switch
+            {
+                TextSentiment.Positive => SentimentType.Positive,
+                TextSentiment.Negative => SentimentType.Negative,
+                TextSentiment.Neutral => SentimentType.Neutral,
+                TextSentiment.Mixed => SentimentType.Mixed,
+                _ => SentimentType.Neutral
+            };
+        }
+        catch
+        {
+            return SentimentType.Neutral;
+        }
+    }
+}
