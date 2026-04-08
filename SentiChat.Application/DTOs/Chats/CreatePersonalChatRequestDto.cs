acb268c93d601e70bef3a9929e9d88f4aaafc875
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 19:34:45 +0300

    Add CreateChat request and response DTOs

---
+++ SentiChat.Application/DTOs/Chats/CreatePersonalChatRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SentiChat.Application.DTOs.Chats;
+
+/// <summary>
+/// Represents the data required to open or create a personal chat.
+/// </summary>
+public record CreatePersonalChatRequestDto
+{
+    /// <summary>
+    /// The unique identifier of the user with whom the personal chat will be initiated.
+    /// </summary>
+    public string TargetUserEmail { get; init; } = string.Empty;
+}
---
+++ SentiChat.Application/DTOs/Chats/PersonalChatCreatedResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SentiChat.Application.DTOs.Chats;
+
+/// <summary>
+/// Represents the data required to open or create a personal chat.
+/// </summary>
+public record PersonalChatCreatedResponseDto
+{
+    /// <summary>
+    /// The unique identifier of the user with whom the personal chat will be initiated.
+    /// </summary>
+    public Guid ChatId { get; init; }
+}
