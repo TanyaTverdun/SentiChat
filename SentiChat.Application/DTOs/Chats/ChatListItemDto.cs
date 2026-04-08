efc03dbde0e887c1c5c87dcd14d41e7dabf3cdcb
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 12:09:30 +0300

    Create ChatListItemDto

---
+++ SentiChat.Application/DTOs/Chats/ChatListItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SentiChat.Application.DTOs.Chats;
+
+public class ChatListItemDto
+{
+    public Guid ChatId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Initials { get; set; } = string.Empty;
+    public string? LastMessageText { get; set; }
+    public DateTime? LastMessageTime { get; set; }
+}
