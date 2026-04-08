1b753e2105dd96beb94b0e5d2af6cef0b918ce2c
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-08 14:20:15 +0300

    Add static factory for Chat

--- SentiChat.Domain/Entities/Chat.cs
+++ SentiChat.Domain/Entities/Chat.cs
@@ -8,4 +8,30 @@ public class Chat
 
     public ICollection<ChatMember> Members { get; set; } = new List<ChatMember>();
     public ICollection<Message> Messages { get; set; } = new List<Message>();
+
+    public static Chat CreatePersonal(
+        Guid user1Id,
+        Guid user2Id)
+    {
+        var chat = new Chat
+        {
+            Id = Guid.NewGuid(),
+            IsGroup = false,
+            Title = null
+        };
+
+        chat.Members.Add(new ChatMember
+        {
+            ChatId = chat.Id,
+            UserId = user1Id
+        });
+
+        chat.Members.Add(new ChatMember
+        {
+            ChatId = chat.Id,
+            UserId = user2Id
+        });
+
+        return chat;
+    }
 }
