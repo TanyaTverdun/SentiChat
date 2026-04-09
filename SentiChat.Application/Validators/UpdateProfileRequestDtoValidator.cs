bb6f69fc10f89ee956bbe9397ec4e403c32a964f
TanyaTverdun <tetiana.tverdun.pz.2023@lpnu.ua>
2026-04-09 19:09:10 +0300

    Add UpdateProfileRequestDtoValidator

---
+++ SentiChat.Application/Validators/UpdateProfileRequestDtoValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using SentiChat.Application.DTOs.Users;
+
+namespace SentiChat.Application.Validators;
+
+/// <summary>
+/// Validates the request data for updating a user profile.
+/// </summary>
+public class UpdateProfileRequestDtoValidator : AbstractValidator<UpdateProfileRequestDto>
+{
+    /// <summary>
+    /// Validates the request data for updating a user profile.
+    /// </summary>
+    public UpdateProfileRequestDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(100)
+            .WithMessage("Name cannot exceed 100 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Invalid email format.")
+            .MaximumLength(255)
+            .WithMessage("Email cannot exceed 255 characters.");
+
+        RuleFor(x => x.Bio)
+            .MaximumLength(500)
+            .WithMessage("Bio cannot exceed 500 characters.");
+    }
+}
